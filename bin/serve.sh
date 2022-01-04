ff0bdddd14ff688136f68bd7c8602fc6af5d3c82
Kevin DeLoach <kdeloach@gmail.com>
2022-01-03 22:50:45 -0500

    Add livereload

--- bin/serve.sh
+++ bin/serve.sh
@@ -3,5 +3,6 @@
 docker run -it --rm \
     -v "$PWD":/usr/src/app \
     -p "4000:4000" \
+    -p "35729:35729" \
     github-pages \
-    jekyll serve -d /_site --watch --force_polling -H 0.0.0.0 -P 4000 --drafts
+    jekyll serve -d /_site --livereload --livereload-port 35729 --host 0.0.0.0 --port 4000 --drafts
